6a20efaeaec348cac0c1e59b084b37ebcd1303b2
Javier Reyes <javier.reys@gmail.com>
2022-08-09 17:14:57 -0400

    feat: add eslint with prettier

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,4 +1,4 @@
-import { createReactQueryHooks } from "@trpc/react";
+import { createReactQueryHooks } from '@trpc/react'
 
 // TODO: add appRouter as generic
-export const trpc = createReactQueryHooks();
+export const trpc = createReactQueryHooks()
