5dc2b96ebf2362239f7ce0d7e437b5a53116ef6c
Javier Reyes <javier.reys@gmail.com>
2022-08-09 15:58:20 -0400

    chore: add config for tRPC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,49 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { withTRPC } from "@trpc/next";
+import { loggerLink } from "@trpc/client/links/loggerLink";
+import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
+import superjson from "superjson";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+// TODO: add appRouter as generic
+export default withTRPC({
+  config({ ctx }) {
+    const url = process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+      : "http://localhost:3000/api/trpc";
+
+    const links = [
+      loggerLink(),
+      httpBatchLink({
+        maxBatchSize: 10,
+        url,
+      }),
+    ];
+
+    return {
+      queryClientConfig: {
+        defaultOptions: {
+          queries: {
+            staleTime: 60,
+          },
+        },
+      },
+      headers() {
+        if (ctx?.req) {
+          return {
+            ...ctx.req.headers,
+            "x-ssr": "1",
+          };
+        }
+        return {};
+      },
+      links,
+      transformer: superjson,
+    };
+  },
+  ssr: false,
+})(MyApp);
